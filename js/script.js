c10f5e06c0b7713eadae98ce307a93d68e7172ff
pierobramanti <piero.bramanti94@gmail.com>
2024-07-18 16:52:47 +0200

    [UPD] Milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
 const { createApp } = Vue;
 
-
-createApp ({
-    data(){
-        return {
-            todo :[
-                {
-                    text: "andare dal medico",
-                    done: false
-
-                },
-                {
-                    text: "comprare il regalo per la fidanzata",
-                    done: false
-                },
-                {
-                    text: "stampare i biglietti dell'aereo",
-                    done: false
-                },
-                {
-                    text: "andare in palestra",
-                    done: false
-                },
-                {
-                    text: "tagliare la barba",
-                    done: false
-                },
-                {
-                    text:"preparare la cena",
-                    done:false
-                }
-            ]
-
-        }
-    },
-
+createApp({
+  data() {
+    return {
+      todo: [
+        {
+          text: "andare dal medico",
+          done: false
+        },
+        {
+          text: "comprare il regalo per la fidanzata",
+          done: false
+        },
+        {
+          text: "stampare i biglietti dell'aereo",
+          done: false
+        },
+        {
+          text: "andare in palestra",
+          done: false
+        },
+        {
+          text: "tagliare la barba",
+          done: false
+        },
+        {
+          text: "preparare la cena",
+          done: false
+        },
+      ],
+    }
+  },
+  methods: {
+    ListDone(i) {
+      this.todo[i].done = true;
+      console.log(this.todo[i].done);
+    }
+  }
 }).mount('#app');
