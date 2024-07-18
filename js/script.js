266cac54edfede25ef6f77dccaade831744f304d
pierobramanti <piero.bramanti94@gmail.com>
2024-07-18 16:05:53 +0200

    [UPD] created object's array and added in application

--- js/script.js
+++ js/script.js
@@ -4,6 +4,33 @@ const { createApp } = Vue;
 createApp ({
     data(){
         return {
+            todo :[
+                {
+                    text: "andare dal medico",
+                    done: false
+
+                },
+                {
+                    text: "comprare il regalo per la fidanzata",
+                    done: false
+                },
+                {
+                    text: "stampare i biglietti dell'aereo",
+                    done: false
+                },
+                {
+                    text: "andare in palestra",
+                    done: false
+                },
+                {
+                    text: "tagliare la barba",
+                    done: false
+                },
+                {
+                    text:"preparare la cena",
+                    done:false
+                }
+            ]
 
         }
     },
